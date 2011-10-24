6bece8de0c3ec6f1cac9782ab657beb0e61c1e64
Tom Janssens <Tom@corebvba.be>
2011-10-24 13:29:06 +0200

    * Renamed web project

--- Example.Web/Properties/AssemblyInfo.cs
+++ Example.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scritchy.Web")]
+[assembly: AssemblyTitle("Example.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Scritchy.Web")]
+[assembly: AssemblyProduct("Example.Web")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
